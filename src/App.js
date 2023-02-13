0f11e8446767213814b60856078c98bb7d4dc9c4
dinhphu <phuongdinhphu41@gmail.com>
2023-02-13 21:26:16 +0700

    change file app.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import OurStore from "./pages/OurStore";
 function App() {
   return (
     <>
@@ -10,6 +12,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="product" element={<OurStore />} />
           </Route>
         </Routes>
       </BrowserRouter>
