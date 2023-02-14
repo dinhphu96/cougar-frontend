f239d7bf463273441daf60f968e3c0ba02066a23
dinhphu <phuongdinhphu41@gmail.com>
2023-02-14 17:08:08 +0700

    add

--- src/App.js
+++ src/App.js
@@ -7,12 +7,18 @@ import Contact from "./pages/Contact";
 import Login from "./pages/Login";
 import OurStore from "./pages/OurStore";
 import Blog from "./pages/Blog";
+import SingleBlog from "./pages/SingleBlog";
 import Cart from "./pages/Cart";
 import Wishlist from "./pages/Wishlist";
 import Forgotpassword from "./pages/Forgotpassword";
 import Signup from "./pages/Signup";
 import Resetpassword from "./pages/Resetpassword";
 import About from "./pages/About";
+import CompareProduct from "./pages/CompareProduct";
+import Checkout from "./pages/Checkout";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+import SingleProduct from "./pages/SingleProduct";
+
 function App() {
   return (
     <>
@@ -24,12 +30,17 @@ function App() {
             <Route path="contact" element={<Contact />} />
             <Route path="login" element={<Login />} />
             <Route path="product" element={<OurStore />} />
+            <Route path="product/:id" element={<SingleProduct />} />
             <Route path="blogs" element={<Blog />} />
+            <Route path="blog/:id" element={<SingleBlog />} />
             <Route path="cart" element={<Cart />} />
             <Route path="wishlist" element={<Wishlist />} />
             <Route path="forgot-password" element={<Forgotpassword />} />
             <Route path="signup" element={<Signup />} />
             <Route path="reset-password" element={<Resetpassword />} />
+            <Route path="compare-product" element={<CompareProduct />} />
+            <Route path="checkout" element={<Checkout />} />
+            <Route path="privacy-policy" element={<PrivacyPolicy />} />
           </Route>
         </Routes>
       </BrowserRouter>
